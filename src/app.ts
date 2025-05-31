086dc1b62a7f9967215ef5c73f482be5e7293469
unknown <neerajj9@gmail.com>
2025-05-31 14:54:21 +0530

    Database setup complete

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(errorHandler)   
+app.use(errorHandler);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
-import app from "./app";
-import config from "./config/config";
+import app from './app';
+import config from './config/config';
+import prisma from './lib/prisma';
+
+async function checkMongoDBConnection(): Promise<void> {
+  try {
+    await prisma.$connect();
+    console.log('✅ Successfully connected to MongoDB using Prisma.');
+  } catch (error) {
+    console.error('❌ Failed to connect to MongoDB using Prisma.');
+    console.error(error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
 
 app.listen(config.port, () => {
-    console.log(`Server listening on port ${config.port}`);
-})
+  console.log(`Server listening on port ${config.port}`);
+  checkMongoDBConnection();
+});
